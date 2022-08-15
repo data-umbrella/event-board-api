e0c402e627f51c06717eccdd73bc26116951dc31
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-14 21:17:39 -0700

    Add sign out endpoint.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CurrentUserView
+from .views import CurrentUserView, sign_out
 
 urlpatterns = [
     path('current_user/', CurrentUserView.as_view()),
+    path('sign_out/', sign_out, name='sign_out'),
 ]
