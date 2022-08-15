2b0cc9a1b318d3c959ffb5e0fce54968b940e121
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-14 17:21:53 -0700

    Update seeds logic to use new data attributes. (#42)

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'authentication.context_processors.passwordless_email_processor',
             ],
         },
     },
