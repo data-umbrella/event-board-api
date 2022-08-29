c7fe10f0739eedbfd6dd7d168f8a708744fc5c5a
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-28 17:05:18 -0700

    WIP

--- events/admin.py
+++ events/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from events.models import Event
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = (
       "event_name",
-      "organization_name",
       "start_date",
-      "end_date",
+      "featured",
+      "published",
+      "author",
+      "review_link",
     )
+    readonly_fields = ('author',)
 
     def identifier(self, obj):
         return self.event_name
+    
+    
 
 
 admin.site.register(Event, EventAdmin)
