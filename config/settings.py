49a60792aa60197657556e92ef228d286fdf6b3a
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-05 13:58:38 -0700

    Update settings to support pagination.

--- config/settings.py
+++ config/settings.py
@@ -183,6 +183,8 @@ REST_FRAMEWORK = {
      'DEFAULT_FILTER_BACKENDS': [
        'django_filters.rest_framework.DjangoFilterBackend',
      ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 if DEVELOPMENT_MODE:
