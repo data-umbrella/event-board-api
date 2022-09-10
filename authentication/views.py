ef82f6ee4d4c80e17c58e1f8203bcf1328fa05b2
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-10 15:53:02 -0700

    Remove unnecessary methods.

--- authentication/views.py
+++ authentication/views.py
@@ -28,58 +28,43 @@ def get_user_from_token(token):
       return None
 
 
-def render_success_response(token, user):
-    # Initial response object
-    response = Response()
-
-    # Set response status
-    response.status = status.HTTP_201_CREATED
-
-    # Set HTTP headers
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
-
-    # Set HTTP only cookies
-    response.set_cookie(
-        'access_token',
-        value=token,
-        max_age=(3600 * 24 * 14),
-        expires=None,
-        httponly=True,
-        samesite='None',
-        domain=settings.AUTH_COOKIE_DOMAIN,
-        secure=True,
-    )
-
-    # Set response data
-    response.data = {
-        'email': user.email,
-        'is_staff': user.is_staff,
-        'id': user.id,
-        'email_verified': user.email_verified,
-    }
-
-    # Return final response
-    return response
-
-
-def render_error_response():
-    return Response({ 'message': 'something went wrong' }, status=status.HTTP_401_UNAUTHORIZED)
-
-
 class CurrentUserView(ObtainAuthToken):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
         token = get_token_from_request(request)
         user = get_user_from_token(token)
+        response = Response()
 
         if user is None:
-            return render_error_response()
+            response.status = status.HTTP_401_UNAUTHORIZED
+            response.data = { 'message': 'something went wrong' }
         else:
-            return render_success_response(token, user)
+            response.status = status.HTTP_201_CREATED
+            response.data = {
+                'email_verified': user.email_verified,
+                'email': user.email,
+                'id': user.id,
+                'is_staff': user.is_staff,
+            }
+
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+            response['Access-Control-Max-Age'] = '1000'
+            response['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type'
+
+            response.set_cookie(
+                'access_token',
+                value=token,
+                max_age=(3600 * 24 * 14),
+                expires=None,
+                httponly=True,
+                samesite='None',
+                domain=settings.AUTH_COOKIE_DOMAIN,
+                secure=True,
+            )
+
+        return response
 
 
 # DELETE /api/sign_out
