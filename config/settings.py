0b2002914ff97f98c9b51f163241f8efc0115a55
Melody Tribble <mjtribble2@gmail.com>
2022-11-18 13:34:51 -0700

    Add button to send weekly digest email to django admin (#86)
    * add 'Send Digest Email' button to django admin
    
    * add success and failure messages

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,9 @@ import dj_database_url
 import os
 import sys
 import environ
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +47,7 @@ DYNAMIC_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 
 if DEVELOPMENT_MODE:
     STATIC_HOSTS = ['localhost']
+    DEBUG = 'True'
 else:
     STATIC_HOSTS = [os.getenv('CLIENT_HOST')]
 
@@ -165,7 +168,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
--- weekly_digest/admin.py
+++ weekly_digest/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import path
+from django.template.response import TemplateResponse
+from django.http import HttpResponseRedirect
+from weekly_digest.utils import trigger_digest_email
 
-# Register your models here.
+from django.utils.safestring import mark_safe
+
+from weekly_digest.models import WeeklyDigestSubscription
+
+class WeeklyDigestSubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+      'email',
+      'subscribed',
+    )
+
+    list_filter = ('email', 'subscribed')
+
+    change_list_template = 'admin/weekly_digests/weekly_digests_change_list.html'
+    
+    def identifier(self, _obj):
+      return self.email
+
+    def send_email(self, request):
+      email_sent = trigger_digest_email()
+      if (email_sent):
+          self.message_user(request, "✨ The Weekly Digest email was successfully sent to all subscribers!")
+      else:
+          self.message_user(request, "🚨 The Weekly Digest email failed to send")
+
+      return HttpResponseRedirect("../")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('send_email/', self.send_email)
+        ]
+        return custom_urls + urls
+    
+    
+admin.site.register(WeeklyDigestSubscription, WeeklyDigestSubscriptionAdmin)
